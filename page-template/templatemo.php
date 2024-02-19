2559662f01ba3e3ef18eda6f72c6791ed43857b3
dishowrov <dinislamshowrov.2001@gmail.com>
2024-02-19 11:48:09 +0600

    new section added

--- page-template/templatemo.php
+++ page-template/templatemo.php
@@ -7,6 +7,7 @@ get_header();
 get_template_part("hero");
 get_template_part("about");
 get_template_part("informations");
+get_template_part("experiences");
 get_template_part("services");
 get_template_part("projects");
 get_template_part("contact");
